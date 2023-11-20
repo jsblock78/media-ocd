20f7ac8bdc67762bfbcd8d70bd5a0e329e6fb7e7
jsblock78 <jsblock78@outlook.com>
2023-11-20 14:58:24 -0700

    Script initialization

--- media-ocd.ps1
+++ media-ocd.ps1
@@ -3,6 +3,28 @@
 Scans media folders to assess consistency.
 #>
 
-$scriptName = $MyInvocation.MyCommand.Name
+[CmdletBinding()]
+param()
 
-Write-Host "$scriptName complete."
+$initialInformationPreference = $InformationPreference
+$InformationPreference = "Continue"
+
+try {
+    # script variables
+    $path = Get-Location
+    $scriptPath = $MyInvocation.MyCommand.Source
+    $scriptName = $MyInvocation.MyCommand.Name
+
+    # Initialization
+    Write-Verbose "Initializing script..."
+    Write-Debug "path: $path"
+    Write-Debug "scriptPath: $scriptPath"
+    Write-Debug "scriptName: $scriptName"
+
+    Write-Information "$scriptName complete."
+
+}
+finally {
+    $InformationPreference = $initialInformationPreference
+}
+
