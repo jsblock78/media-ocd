c49184cce39dc9cb82cd94ef83f663f746502e1e
jsblock78 <jsblock78@outlook.com>
2023-11-23 07:54:21 -0700

    Add MediaPath, NonInteractive and Quiet parameters

--- media-ocd.ps1
+++ media-ocd.ps1
@@ -1,30 +1,79 @@
 <#
 .SYNOPSIS
 Scans media folders to assess consistency.
+
+.PARAMETER MediaPath
+The path to the root of the media repository.
+
+.PARAMETER NonInteractive
+If true this script will not prompt the user for input.
+
+.PARAMETER Quiet
+If true this script will not output informational text. Verbose or Debug messages will be output if their corresponding preferences are set.
+
 #>
 
 [CmdletBinding()]
-param()
+param(
+    [string]$MediaPath = $env:JSB_MediaPath,
+    [switch]$NonInteractive = $false,
+    [switch]$Quiet = $false
+)
 
-$initialInformationPreference = $InformationPreference
-$InformationPreference = "Continue"
 
 try {
+    ##########################################################################
+    # Initialization
+    ##########################################################################
+    Write-Verbose "Initializing script..."
+    
     # script variables
     $path = Get-Location
-    $scriptPath = $MyInvocation.MyCommand.Source
-    $scriptName = $MyInvocation.MyCommand.Name
-
-    # Initialization
-    Write-Verbose "Initializing script..."
     Write-Debug "path: $path"
-    Write-Debug "scriptPath: $scriptPath"
+    $scriptName = $MyInvocation.MyCommand.Name
     Write-Debug "scriptName: $scriptName"
+    $scriptPath = $MyInvocation.MyCommand.Source
+    Write-Debug "scriptPath: $scriptPath"
+    $scriptRoot = $PSScriptRoot
+    Write-Debug "scriptRoot: $scriptRoot"
+
+    # process Quiet parameter
+    if (-Not $Quiet) {
+        $initialInformationPreference = $InformationPreference
+        $InformationPreference = "Continue"
+    }
+
+    # process MediaPath parameter
+    if (-Not $MediaPath) {
+        if ($NonInteractive) {
+            Throw "MediaPath parameter is required."
+        } else {
+            $MediaPath = Read-Host "Enter path to Media, press Enter for [$path]"
+            if ($MediaPath -eq "") { $MediaPath = $path }
+        }
+    }
+
+    # write initialization debug
+    Write-Debug "MediaPath: $MediaPath"
+    
+    ##########################################################################
+    # Validate Media Structure
+    ##########################################################################
+
+
+    ##########################################################################
+    # Cleanup
+    ##########################################################################
+
+    # Remove ehthumbs.db
 
     Write-Information "$scriptName complete."
 
 }
 finally {
-    $InformationPreference = $initialInformationPreference
+    if (-Not $Quiet) {
+        # reset information preference
+        $InformationPreference = $initialInformationPreference
+    }
 }
 
